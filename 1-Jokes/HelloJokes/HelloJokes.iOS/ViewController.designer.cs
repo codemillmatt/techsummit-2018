d9b878a198796ce344531b2f6804a7a3d086fc44
Matt Soucoup <msoucoup@codemilltech.com>
2018-02-22 11:49:40 -0600

    iOS and Droid

---
+++ 1-Jokes/HelloJokes/HelloJokes.iOS/ViewController.designer.cs
@@ -0,0 +1,37 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace HelloJokes.iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton getJoke { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel jokeText { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (getJoke != null) {
+                getJoke.Dispose ();
+                getJoke = null;
+            }
+
+            if (jokeText != null) {
+                jokeText.Dispose ();
+                jokeText = null;
+            }
+        }
+    }
+}
---
+++ 1-Jokes/HelloJokes/HelloJokes/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using HelloJokes.Core;
+
+namespace HelloJokes
+{
+    [Activity(Label = "HelloJokes", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var getJoke = FindViewById<Button>(Resource.Id.getJoke);
+            var jokeText = FindViewById<TextView>(Resource.Id.joke);
+
+            getJoke.Click += async (sender, e) =>
+            {
+                var jokeService = new JokeService();
+                var theJoke = await jokeService.GetJoke();
+                jokeText.Text = theJoke.Joke;
+            };
+        }
+    }
+}
+
